ccacde7cf0d3bfb9129c1402dad45fe666f867f2
Colin Uken <colin.uken@gmail.com>
2023-06-20 16:38:51 -0700

    feat: add flake.nix

---
+++ flake.nix
@@ -0,0 +1,45 @@
+{
+  description = "Cuken's NixOS and MacOS configuration";
+
+  inputs = {
+    nixpkgs = {
+      url = "github:cuken/nixpkgs/master";
+    };
+    home-manager = {
+      url = "github:nix-community/home-manager";
+    };
+    darwin = {
+      url = "github:LnL7/nix-darwin/master";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs = { self, flake-utils, darwin, home-manager, nixpkgs, ...}@inputs: {
+    darwinConfigurations = {
+      "AA-MBP" = darwin.lib.darwinSystem {
+        system = "aarch64-darwin";
+        modules = [
+          ./darwin
+        ];
+        inputs = { inherit darwin home-manager nixpkgs; };
+      };
+    };
+    nixosConfigurations = {
+      fenrir = nixpkgs.lib.nixosSystem {
+        system = "x64_64-linux";
+        modules = [
+          ./nixos
+          ./hardware/fenrir.nix
+          home-manager.nixosModules.home-manager {
+            home-manager.useGlobalPkgs = true;
+            home-manager.useUserPackages = true;
+            home-manager.users.cuken = import ./nixos/home-manager.nix;
+          }
+        ];
+        specialArgs = {
+          inherit inputs;
+        };
+      };
+    };
+  };
+}
